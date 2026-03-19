527767d97900f2e26d1c329e2b303a4bd6b87c42
Arthur Chuang <acychuang229@gmail.com>
2026-03-19 17:42:33 +0800

    Add voice ID

--- vocab.js
+++ vocab.js
@@ -227,7 +227,7 @@ function shuffle(array) {
 }
 
 const ELEVENLABS_API_KEY = "sk_a65d12b3e10312ce5ea5c72b310f73ed36dab713df0bded4";
-const VOICE_ID = "0ptCJp0xgdabdcpVtCB5";
+const VOICE_ID = "3JDquces8E8bkmvbh6Bc";
 const MODEL_ID = "eleven_multilingual_v2";
 
 // Cache to avoid re-calling the API for the same word
